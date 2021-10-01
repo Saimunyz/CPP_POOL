70ecc6c096885e0a723626429c0e5b2a1bd8ad95
swagstaf <raning@mail.ru>
2021-10-01 23:30:43 +0300

    not worked ex02

---
+++ day06/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swagstaf <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 21:04:14 by swagstaf          #+#    #+#             */
+/*   Updated: 2021/10/01 23:29:37 by swagstaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+int	roll(int min, int max)
+{
+	double x = rand()/static_cast<double>(RAND_MAX + 1)
+}
+
+Base * generate(void) {
+
+}
+
+int	main()
+{
+
+
+	return 0;
+}
